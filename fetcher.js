65660d0789c8aaf2aa712b7adbb28f8afcc30b39
jon-jh <hiebert456@gmail.com>
2024-07-11 16:05:11 -0600

    downloader working but not from command line input yet

--- fetcher.js
+++ fetcher.js
@@ -14,8 +14,20 @@
 const needle = require('needle');
 const fs = require('fs');
 
-const download = function(url, savePath) {
-  
+const download = function(url, path) {
+  needle.get(url, (error, response) => { // needle MUST have it written this way in order to function.
+    
+    if (response && response.body) { // if the response and .body exists
+      console.log('webpage accessed, downloading');
+      fs.writeFile(path, response.body, () => // this MUST also be a function.
+        console.log(`finished downloading: ${url}, saved ${response.body.length} bytes to ${path}`)
+      );
+    } else {
+      console.log('skipped reading response and body for some reason');
+    }
+  });
 };
+download('https://google.com/', './index.html');
+
 
 
