769b3894cf330115414aaa84a2135d9183de6597
jon-jh <hiebert456@gmail.com>
2024-07-11 14:17:12 -0600

    new code started

---
+++ fetcher.js
@@ -0,0 +1,21 @@
+/* We are trying to make an app that downloads a web page using it's url and a path to save it to.
+
+- A function that will take 2 arguments
+
+- Should look like this - download(pageURL, savePath)
+
+- Use the npm needle library for the HTTP connection ( instead of using the normal 'net' built in to Node.)
+
+- Use Node fs module to write / save the file.
+
+- easy peasy lemon squeezy?
+*/
+
+const needle = require('needle');
+const fs = require('fs');
+
+const download = function(url, savePath) {
+  
+};
+
+
